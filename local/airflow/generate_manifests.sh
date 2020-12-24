7460ab305ff952738fca40c00aec08d2307d8587
Eric Vallee <eric_vallee@webificservices.com>
2020-12-23 21:34:27 -0500

    fix: #196 Added an exec pod to troubleshoot things locally

--- local/airflow/generate_manifests.sh
+++ local/airflow/generate_manifests.sh
@@ -1,3 +1,4 @@
 export CURRENT_DIR=$(pwd);
 envsubst '$CURRENT_DIR' < configsmap-override.yml.tpl > configsmap-override.yml;
-envsubst '$CURRENT_DIR' < deployments-override.yml.tpl > deployments-override.yml
+envsubst '$CURRENT_DIR' < deployments-override.yml.tpl > deployments-override.yml;
+envsubst '$CURRENT_DIR' < deployments.yml.tpl > deployments.yml
