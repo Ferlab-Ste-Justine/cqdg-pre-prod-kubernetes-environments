5ba4df31ff643b168ebdc96f58095453879cda03
adrian <adipaul1981@yahoo.ca>
2022-07-13 14:20:06 -0400

    fix: CQDG-8 rename certificate from crt to pem

--- qa/apps/fhir-server/entrypoint/entrypoint.sh
+++ qa/apps/fhir-server/entrypoint/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-cp /opt/ingress-ca/ca.crt /usr/local/share/ca-certificates/;
+cp /opt/ingress-ca/ca.pem /usr/local/share/ca-certificates/;
 update-ca-certificates;
 
-openssl x509 -in /opt/ingress-ca/ca.crt -inform pem -out /opt/ingress-ca.der -outform der
+openssl x509 -in /opt/ingress-ca/ca.pem -inform pem -out /opt/ingress-ca.der -outform der
 keytool -noprompt -importcert -trustcacerts -cacerts -alias cqgc -storepass changeit -file /opt/ingress-ca.der
 
 exec catalina.sh run;
