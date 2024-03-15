a42dca134cdf0eb6ce6a5c023d4fca88a9c84371
Sébastien Bonami <sbonami@ferlab.bio>
2024-03-15 09:08:59 -0400

    feat: INFRA-594 remove qa folder not used anymore

--- qa/apps/arranger/configure-arranger/readme.md
+++
@@ -1,5 +0,0 @@
-### How to retrieve arranger configuration
-
-wget http://localhost:9200/arranger-projects-cqdg/_search?q=*
-
-For each "hit", copy the content of the "_source" field into the corresponding configmap. 
--- qa/apps/fhir-server/entrypoint/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-cp /opt/ingress-ca/ca.pem /usr/local/share/ca-certificates/;
-update-ca-certificates;
-
-openssl x509 -in /opt/ingress-ca/ca.pem -inform pem -out /opt/ingress-ca.der -outform der
-keytool -noprompt -importcert -trustcacerts -cacerts -alias cqgc -storepass changeit -file /opt/ingress-ca.der
-
-exec catalina.sh run;
